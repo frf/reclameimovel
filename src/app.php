7f3d1fbdeec2482043344df2bec7b1acfeb98665
Fabio Rocha <fabio@fabiofarias.com.br>
2014-05-05 02:20:19 -0300

    UserProvider

--- src/app.php
+++ src/app.php
@@ -78,7 +78,7 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
                 'logout_path' => '/logout',
                 'with_csrf' => true
             ),
-            'users' => new OAuthUserProvider()
+            'users' => new \Condominio\Repository\OAuthUserProvider()
         )
     ),
     'security.access_rules' => array(
