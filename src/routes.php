fa49ad9c16f7c607a454992be408bb2f69b24b83
fabio Rocha <fabio@fabiofarias.com.br>
2014-05-07 15:23:58 -0300

    Rota erro

--- src/routes.php
+++ src/routes.php
@@ -6,5 +6,5 @@ $app->get('/morador', 'Condominio\Controller\MoradorController::indexAction')->b
 $app->get('/adicionar/{ide}', 'Condominio\Controller\MoradorController::adicionarAction')->bind('reclamacao_add')->value('ide',false);
 $app->get('/construtora', 'Condominio\Controller\IndexController::construtoraAction')->bind('construtora');
 $app->get('/{ide}', 'Condominio\Controller\IndexController::indexAction')->bind('principal')->value('ide',false);
-$app->get('/{ide}/{id}', 'Condominio\Controller\IndexController::viewAction')->bind('view')->value('ide',false)->value('id',false);
+$app->get('/view/{ide}/{id}', 'Condominio\Controller\IndexController::viewAction')->bind('view')->value('ide',false)->value('id',false);
 $app->post('/adicionar', 'Condominio\Controller\MoradorController::adicionarAction');
