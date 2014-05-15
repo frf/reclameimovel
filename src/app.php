bf422bda359e9ab90d1cadac2a39aff3f425906f
Fabio Rocha <fabio@fabiofarias.com.br>
2014-05-15 15:28:41 -0300

    Envio de imagens

--- src/app.php
+++ src/app.php
@@ -3,7 +3,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
-$app['view_path'] = 'http://localhost:8000/view';
+$app['view_path'] = 'http://reclameimove.com.br/view';
     
 $app['repository.empresa'] = $app->share(function ($app) {
     return new Condominio\Repository\EmpresaRepository($app['db']);
