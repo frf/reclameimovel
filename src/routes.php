0b0652ad093d32889ee2f29d93c12763f40e3a86
Fabio Rocha <fabio@fabiofarias.com.br>
2014-05-17 15:03:32 -0300

    Acerto de login

--- src/routes.php
+++ src/routes.php
@@ -13,10 +13,8 @@ $app->get('/{idnome}', 'Condominio\Controller\IndexController::indexAction')->bi
 $app->get('/view/{ide}/{id}', 'Condominio\Controller\IndexController::viewAction')->bind('view')->value('ide',false)->value('id',false);
 $app->get('/api/empreendimento', 'Condominio\Controller\EmpreendimentoController::apiAction')->bind('api_emp');
 
-
-
 $app->post('/buscar', 'Condominio\Controller\IndexController::buscarAction')->bind('buscar');
 $app->post('/adicionar', 'Condominio\Controller\MoradorController::adicionarAction');
 $app->post('/adicionar/foto', 'Condominio\Controller\MoradorController::adicionarFotoAction');
-
+$app->post('/morador/dados-complementares', 'Condominio\Controller\MoradorController::dadosAction')->bind('dados_usuario');
 
