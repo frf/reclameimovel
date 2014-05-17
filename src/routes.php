00bac40ed600ece605014cdb10a09472805ab83d
Fabio Rocha <fabio@fabiofarias.com.br>
2014-05-17 15:05:48 -0300

    Acerto de login

--- src/routes.php
+++ src/routes.php
@@ -16,5 +16,5 @@ $app->get('/api/empreendimento', 'Condominio\Controller\EmpreendimentoController
 $app->post('/buscar', 'Condominio\Controller\IndexController::buscarAction')->bind('buscar');
 $app->post('/adicionar', 'Condominio\Controller\MoradorController::adicionarAction');
 $app->post('/adicionar/foto', 'Condominio\Controller\MoradorController::adicionarFotoAction');
-$app->post('/morador/dados-complementares', 'Condominio\Controller\MoradorController::dadosAction')->bind('dados_usuario');
+$app->post('/morador/dados-complementares/adicionar', 'Condominio\Controller\MoradorController::dadosAction')->bind('dados_usuario');
 
