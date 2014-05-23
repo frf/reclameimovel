8482b60516325c01c5457702749a0a139c744a69
Fabio Rocha <fabio@fabiofarias.com.br>
2014-05-23 00:18:38 -0300

    Facebok session

--- src/Condominio/Repository/FacebookRepository.php
+++ src/Condominio/Repository/FacebookRepository.php
@@ -16,19 +16,35 @@ use Facebook\FacebookCanvasLoginHelper;
  */
 class FacebookRepository
 {
-   var $key = "CAADXo9oqDpgBAFH4Lp4voyizEWTO7mCvYKMz3ZCdxjKTCT5VO0k518eUHMubxzNc3pHadVBgMvLdpBQBQZAf3beFw2SOOKAtuZARi19IqIaxldjPIoMYz6oGaMNx7QVa8Y1GBHXA9dOiiV1dgVieZBpV7mE2fJZA3z5qd6VHih0PcK6ZB9xIPRPFH2FyPuJGAZD";
+   //var $key = "CAADXo9oqDpgBAFH4Lp4voyizEWTO7mCvYKMz3ZCdxjKTCT5VO0k518eUHMubxzNc3pHadVBgMvLdpBQBQZAf3beFw2SOOKAtuZARi19IqIaxldjPIoMYz6oGaMNx7QVa8Y1GBHXA9dOiiV1dgVieZBpV7mE2fJZA3z5qd6VHih0PcK6ZB9xIPRPFH2FyPuJGAZD";
    public function __construct($app) {
-        FacebookSession::setDefaultApplication('237098739830424', '1269e70d03f172e6e2ccf67e6c4f10f0');
+        FacebookSession::setDefaultApplication('237093413164290', '8f94031a4b4a962543c33747c1a2e6e7');
+        
+        
+        $helper = new FacebookCanvasLoginHelper();
+        try {
+          $session = $helper->getSession();
+        } catch(FacebookRequestException $ex) {
+          // When Facebook returns an error
+        } catch(\Exception $ex) {
+          // When validation fails or other local issues
+        }
+        if ($session) {
+          // Logged in
+            var_dump($session);
+        }
+
+
         
         // If you already have a valid access token:
-        $this->session = new FacebookSession($this->key);
-        $this->request = new FacebookRequest($this->session, 'GET', '/me');
+        #$this->session = new FacebookSession($this->key);
+       # $this->request = new FacebookRequest($this->session, 'GET', '/me');
         
         // If you're making app-level requests:
         //$session = FacebookSession::newAppSession();
 
         // To validate the session:
-        try {
+        /*try {
           $this->session->validate();
         } catch (FacebookRequestException $ex) {
           // Session not valid, Graph API returned an exception with the reason.
@@ -37,7 +53,7 @@ class FacebookRepository
           // Graph API returned info, but it may mismatch the current app or have expired.
           echo $ex->getMessage();
         }
-        
+        */
         
    }
    public function checkSession(){
